e7f415ab9f90ad5527f768e7fc06a2d9ec5bc9ff
Yongwoo Yu (Yowu) <me@yowu.dev>
2025-11-30 16:04:23 +0900

    외부 의존성 제거

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,23 @@
-/*
-	Visualize by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
+(function() {
+	"use strict";
 
-$(function() {
+	// Core Utils
+	var on = function(element, event, handler) {
+		element.addEventListener(event, handler);
+	};
 
-	// Vars.
-		var	$window = $(window),
-			$body = $('body'),
-			$wrapper = $('#wrapper');
-
-	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+	// Vars
+	var body = document.body;
 
 	// Disable animations/transitions until everything's loaded.
-		$body.addClass('is-loading');
+	body.classList.add('is-loading');
 
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
+	on(window, 'load', function() {
+		// Remove loading class
+		// 약간의 딜레이를 주어 부드럽게 전환되도록 함
+		setTimeout(function() {
+			body.classList.remove('is-loading');
+		}, 100);
+	});
 
-	// Poptrox.
-		$window.on('load', function() {
-
-			$('.thumbnails').poptrox({
-				onPopupClose: function() { $body.removeClass('is-covered'); },
-				onPopupOpen: function() { $body.addClass('is-covered'); },
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: true,
-				overlayColor: '#000000',
-				overlayOpacity: 0.75,
-				popupLoaderText: '',
-				fadeSpeed: 500,
-				usePopupDefaultStyling: false,
-				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
-			});
-
-		});
-
-});
+})();
